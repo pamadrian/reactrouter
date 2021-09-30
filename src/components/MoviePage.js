92c3752343c6797a39b0d286af04bb8c5d3fb443
pamadrian <pamvanalfen@icloud.com>
2021-09-30 17:44:04 +0200

    styling part

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import "./MoviePage.scss";
 
 export default function MoviePage() {
   const [movie, setMovie] = useState({});
@@ -21,11 +22,34 @@ export default function MoviePage() {
   //   I forgot to place the imdb_id at the array, run it to show in the page
 
   return (
-    <div>
-      <h1>Movie Page</h1>
-      <h3>{movie.Title}</h3>
-      <img src={movie.Poster} alt={movie.Title} />
-      <p>{movie.Plot}</p>
+    <div className="MoviePage">
+      {movie ? (
+        <>
+          <h2>{movie.Title}</h2>
+          {/* <div>
+            {movie.Genre.split(", ").map((genre, index) => (
+              <span key={index} className="genre">
+                {genre}
+              </span>
+            ))}
+          </div> */}
+          <div className="details">
+            <img alt={movie.Title} src={movie.Poster} />
+            <div>
+              <h3>Director</h3>
+              <p>{movie.Director}</p>
+              <h3>Language</h3>
+              <p>{movie.Language}</p>
+              <h3>Plot</h3>
+              <p>{movie.Plot}</p>
+              <h3>IMDB Rating</h3>
+              <p>{movie.imdbRating}</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading....</p>
+      )}
     </div>
   );
 }
